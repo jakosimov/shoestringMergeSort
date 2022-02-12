77ba9636172d3eac0cc7b81f121b2d18cbfb9339
xt0r3 <kgs37@cam.ac.uk>
2022-02-12 16:41:51 +0000

    Commit #1 - added interfaces.py

---
+++ static/assets/python/interfaces.py
@@ -0,0 +1,65 @@
+from abc import ABC, abstractmethod
+
+
+class ScaleInterface(ABC):
+    @abstractmethod
+    def measure(self):
+        pass
+
+
+class DataInterface(ABC):
+    @abstractmethod
+    def getTotalWeight(self):
+        pass
+
+
+class ShelfInterface(ABC):
+    @abstractmethod
+    def getItemCount(self):
+        pass
+
+    @abstractmethod
+    def processNewData(self, data):
+        pass
+
+    @abstractmethod
+    def setItemWeight(self, coeff):
+        pass
+
+    @abstractmethod
+    def getItemName(self):
+        pass
+
+    @abstractmethod
+    def setItemName(self, itemName):
+        pass
+
+    @abstractmethod
+    def initializeShelf(self, scale):
+        pass
+
+
+class InventoryManagementSystemInterface(ABC):
+    @abstractmethod
+    def checkItems(self):
+        pass
+
+    @abstractmethod
+    def plotDemand(self):
+        pass
+
+    @abstractmethod
+    def saveShelfStates(self):
+        pass
+
+    @abstractmethod
+    def getShelfStates(self, timeStamp):
+        pass
+
+    @abstractmethod
+    def getShelfState(self, timeStamp, shelfIndices):
+        pass
+
+    @abstractmethod
+    def raiseAlert(self, shelfIndex):
+        pass
