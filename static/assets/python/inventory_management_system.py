cc3a5b4d3f037a800febd2d4cf2d98039a68c858
xt0r3 <kgs37@cam.ac.uk>
2022-02-12 22:20:42 +0000

    Commit #8: added comments to saveShelfStates()

--- static/assets/python/inventory_management_system.py
+++ static/assets/python/inventory_management_system.py
@@ -27,20 +27,29 @@ class InventoryManagementSystem(InventoryManagementSystemInterface):
         raise NotImplementedError
 
     def saveShelfStates(self) -> None:
+        """
+        A function to save the current state of the shelves into the
+        JSON database pointed to by self.datapath (databases/database.json by default)
+        """
+
+        # IF DATAPATH IS EMPTY, INITIALIZE TO DEFAULT VALUE
         if self.datapath == "":
             self.initializeDatabase()
 
+        # GET CURRENT DATETIME
         current_date = datetime.now()
         date_string = current_date.strftime("%y/%m/%d %H:%M:%S")
 
-        currentState = {
+        # GET CURRENT STATE PARAMETERS OF THE SHELVES (name/amount)
+        currentState: Dict[int, Dict[str, Union[int, str]]] = {
             shelf.getShelfId(): {
                 "name": shelf.getItemName(),
                 "amount": shelf.getItemCount()
             } for shelf in self.shelves.values()
         }
 
-        old_states = {}
+        # TRY TO GET OLD STATES
+        old_states: Dict[str, Dict[int, Dict[str, Union[int, str]]]] = {}
         with open(self.datapath, "r") as data_file:
             try:
                 old_states = json.load(data_file)
@@ -48,11 +57,12 @@ class InventoryManagementSystem(InventoryManagementSystemInterface):
             except JSONDecodeError:
                 pass
 
-        with open(self.datapath, "w") as data_file:
-            old_states[date_string] = currentState
-            json.dump(old_states, data_file, indent=2)
-            data_file.truncate()
-            print(json.dumps(old_states, indent=2))
+        # UPDATE OLD STATES WITH CURRENT STATES
+        with open(self.datapath, "w") as data_file:  # OPEN THE DATABASE
+            old_states[date_string] = currentState  # ADD CURRENT STATE TO OLD STATE
+            json.dump(old_states, data_file, indent=2)  # OVERWRITE THE DATABASE BY THE UPDATED STATES
+            data_file.truncate()  # TRUNCATE TO AVOID ERRORS
+            # print(json.dumps(old_states, indent=2))  DEBUG
 
     def getShelfStates(self, timeStamp: datetime):
         raise NotImplementedError
@@ -63,8 +73,13 @@ class InventoryManagementSystem(InventoryManagementSystemInterface):
     def raiseAlert(self, shelfIndex):
         raise NotImplementedError
 
-    def addShelf(self, shelf: Shelf):
-        self.shelves[shelf.getShelfId()] = shelf
+    def addShelf(self, shelf: Shelf) -> None:
+        """
+        :param shelf: A Shelf to be added to the list of shelves. If there is a shelf with that id, raise exception
+        """
+        if shelf.getShelfId() in self.shelves.keys():  # IF A SHELF WITH THE ID EXISTS, RAISE ERROR
+            raise IndexError
+        self.shelves[shelf.getShelfId()] = shelf  # ADD SHELF TO SHELVES
 
     def removeShelf(self, shelfId):
         self.shelves.pop(shelfId)
