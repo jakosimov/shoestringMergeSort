33297402203a194b7357b521e51b4806b536d6b2
AnMD <amd219@cam.ac.uk>
2022-02-13 13:20:22 +0000

    First attempt at Arduino-Python communication: implemented scale.py

--- scale.py
+++ scale.py
@@ -1,9 +1,29 @@
 from interfaces import ScaleInterface
+import serial
+import time
 
 
 class Scale(ScaleInterface):
     def __init__(self) -> None:
-        pass
+        serial_port = 57600
 
     def measure(self) -> float:
-        raise NotImplementedError
+        # set up the serial line
+        ser = serial.Serial('COM4', self.serial_port)
+        time.sleep(2)
+
+        # read and record the data
+        data = []
+        for i in range(50):
+            b = ser.readline()  # read a byte string
+            string_n = b.decode()  # decode byte string into Unicode
+            string = string_n.rstrip()  # remove \n and \r
+            flt = float(string)
+            print(flt)
+            data.append(flt)  # add to the end of data list
+            time.sleep(0.1)  # wait
+        ser.close()
+
+        # show the data
+        for line in data:
+            print(line)
