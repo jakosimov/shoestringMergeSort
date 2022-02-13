bfdce8482bbe109dbb7c0f14a07f1ded33ac68db
AnMD <amd219@cam.ac.uk>
2022-02-13 01:02:08 +0000

    Rearranged test.py

--- static/test.py
+++ static/test.py
@@ -1,4 +1,5 @@
 from time import sleep
+import unittest
 
 from static.assets.python.shelf import Shelf
 from static.assets.python.inventory_management_system import InventoryManagementSystem
@@ -9,31 +10,32 @@ def fake_scale_measurement() -> float:
     return fake_measurement
 
 
-store = InventoryManagementSystem()
-nameList1 = ["mango", "bubble tea", "I am tired"]
-nameList2 = ["I", "AM", "A", "GENIUS"]
+if __name__ == '__main__':
+    store = InventoryManagementSystem()
+    nameList1 = ["mango", "bubble tea", "I am tired"]
+    nameList2 = ["I", "AM", "A", "GENIUS"]
 
-for i, name in enumerate(nameList1):
-    shelf = Shelf()
-    shelf.setShelfId(i)
-    shelf.setItemName(name)
-    shelf.setItemWeight(100 * (i + 1))
-    shelf.currentWeight = i * (i + 1) * 100 + 3
-    store.addShelf(shelf)
+    for i, name in enumerate(nameList1):
+        shelf = Shelf()
+        shelf.setShelfId(i)
+        shelf.setItemName(name)
+        shelf.setItemWeight(100 * (i + 1))
+        shelf.currentWeight = i * (i + 1) * 100 + 3
+        store.addShelf(shelf)
 
-store.saveShelfStates()
+    store.saveShelfStates()
 
-for i in range(len(nameList1)):
-    store.removeShelf(i)
+    for i in range(len(nameList1)):
+        store.removeShelf(i)
 
-sleep(5)
+    sleep(5)
 
-for i, name in enumerate(nameList2):
-    shelf = Shelf()
-    shelf.setShelfId(i)
-    shelf.setItemName(name)
-    shelf.setItemWeight(100 * (i + 1))
-    shelf.currentWeight = i * (i + 1) * 100 + 3
-    store.addShelf(shelf)
+    for i, name in enumerate(nameList2):
+        shelf = Shelf()
+        shelf.setShelfId(i)
+        shelf.setItemName(name)
+        shelf.setItemWeight(100 * (i + 1))
+        shelf.currentWeight = i * (i + 1) * 100 + 3
+        store.addShelf(shelf)
 
-store.saveShelfStates()
+    store.saveShelfStates()
